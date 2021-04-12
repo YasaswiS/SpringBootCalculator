4a67039a2a009e02d1d3b2ed79c40ab2141ad756
YasaswiS <49824724+YasaswiS@users.noreply.github.com>
2021-04-11 19:23:53 -0700

    Update CalculateSimple.java

--- src/main/java/com/calculator/debugdatabandits/service/CalculateSimple.java
+++ src/main/java/com/calculator/debugdatabandits/service/CalculateSimple.java
@@ -17,7 +17,20 @@ public class CalculateSimple {
     
     private int gcd(int a, int b)
 {
-    return a * b;
+
+    if (a == 0)
+       return b;
+    if (b == 0)
+       return a;
+  
+
+    if (a == b)
+        return a;
+  
+
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
 }
     
 
